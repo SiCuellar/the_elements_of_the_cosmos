eaec104ae282f6a9e79524c4157352613ce5ce6e
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-03-17 18:28:37 -0600

    deploy

--- main.bundle.js
+++ main.bundle.js
@@ -44,8 +44,24 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-	// This file is in the entry point in your webpack config.
 	"use strict";
+
+	// This file is in the entry point in your webpack config.
+	var search = document.querySelector("#search-input");
+
+	function getWeather() {
+	    fetch('https://frozen-lake-45864.herokuapp.com/api/v1/forecast?location=' + search).then(function (response) {
+	        return response.json();
+	    }).then(function (data) {
+	        service(data);
+	    });
+	}
+
+	function current(raw_data) {
+	    current = {
+	        current: raw_data["data"]["attributes"]["predict"]["currently"]
+	    };
+	}
 
 /***/ })
 /******/ ]);
